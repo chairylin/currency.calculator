f0cdac59fb399115034f1a6a59bd8f9239e2db56
chairylin <chairylin@mail.ru>
2021-03-13 14:24:35 +0300

    Update class.php
    merge

--- class.php
+++ class.php
@@ -10,8 +10,8 @@ if(!class_exists('CurrencyCalculator')) {
 
         private $id_list_currency;
         private $id_exchange_rates;
-	private $list_currency;
-	private $exchange_rates;
+        private $list_currency;
+        private $exchange_rates;
 
         function __construct($component = null)
         {
@@ -19,107 +19,107 @@ if(!class_exists('CurrencyCalculator')) {
         }
 
         function getEntityDataClass($HlBlockId)
-	{
-            if (empty($HlBlockId) || $HlBlockId < 1)
-            {
-                return false;
+        {
+                if (empty($HlBlockId) || $HlBlockId < 1)
+                {
+                    return false;
+                }
+                $hlblock = HL\HighloadBlockTable::getById($HlBlockId)->fetch();
+                $entity = HL\HighloadBlockTable::compileEntity($hlblock);
+                $entity_data_class = $entity->getDataClass();
+                return $entity_data_class;
             }
-            $hlblock = HL\HighloadBlockTable::getById($HlBlockId)->fetch();
-            $entity = HL\HighloadBlockTable::compileEntity($hlblock);
-            $entity_data_class = $entity->getDataClass();
-            return $entity_data_class;
+            
+        function getBlockTableList ()
+        {
+            $dbItems = HL\HighloadBlockTable::getList();
+                $list = [];
+
+                while($arRes = $dbItems->fetch()){
+                    $list[$arRes["NAME"]] = $arRes["ID"];
+                }
+                
+            return $list;
         }
-		
-	function getBlockTableList ()
-	{
-	    $dbItems = HL\HighloadBlockTable::getList();
-            $list = [];
+            
+        function getIdHighloadblock()
+        {
+            $list = $this->getBlockTableList();
+                
+            if ( isset($list["ListCurrency"]) ) {
+                    $this->id_list_currency = $list["ListCurrency"];
+                } else {
+                    $this->id_list_currency = $this->createListCurrency();
+                }
 
-            while($arRes = $dbItems->fetch()){
-                $list[$arRes["NAME"]] = $arRes["ID"];
+                if ( isset($list["ExchangeRates"]) ) {
+                    $this->id_exchange_rates = $list["ExchangeRates"];
+                } else {
+                    $this->id_exchange_rates = $this->createExchangeRates();
+                }
+        }
+            
+        function cbrXMLDailyRu()
+        {
+            return json_decode(file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js'), true);
+        }
+            
+        function setRates()
+        {
+            $ar_rates = $this->cbrXMLDailyRu();
+            $date = date('d.m.Y');
+            $entity_data_class = $this->getEntityDataClass($this->id_exchange_rates);
+
+            foreach($this->list_currency as $value){
+
+                $car_rate = $ar_rates["Valute"][$value["UF_SHORT_NAME"]]["Value"]??1;
+
+                $this->exchange_rates[$value["UF_SHORT_NAME"]] = [
+                'UF_VALUE' => $car_rate,
+                ];
+
+                $entity_data_class::add([
+                'UF_CURRENCY_ID' => $value["ID"],
+                'UF_VALUE' => $car_rate,
+                'UF_DATE' => $date,
+                ]);
             }
-			
-	    return $list;
-	}
-		
-	function getIdHighloadblock()
-	{
-	    $list = $this->getBlockTableList();
-			
-	    if ( isset($list["ListCurrency"]) ) {
-                $this->id_list_currency = $list["ListCurrency"];
-            } else {
-                $this->id_list_currency = $this->createListCurrency();
+        }
+            
+        function getRates()
+        {
+            $entity_data_class = $this->getEntityDataClass($this->id_exchange_rates);
+                $rsData = $entity_data_class::getList([
+            "select" => array("UF_CURRENCY_ID", "UF_VALUE"),
+            "order" => array("ID" => "ASC"),
+            "filter" => array("UF_DATE"=>date('d.m.Y'))  // Задаем параметры фильтра выборки
+            ]);
+                
+            while($arData = $rsData->Fetch()){
+                $this->exchange_rates[$this->list_currency[$arData["UF_CURRENCY_ID"]]["UF_SHORT_NAME"]]["UF_VALUE"] = $arData["UF_VALUE"];
             }
-
-            if ( isset($list["ExchangeRates"]) ) {
-                $this->id_exchange_rates = $list["ExchangeRates"];
-            } else {
-                $this->id_exchange_rates = $this->createExchangeRates();
+            
+            if (empty($this->exchange_rates)) {
+                $this->setRates();
             }
-	}
-		
-	function cbrXMLDailyRu()
-	{
-	    return json_decode(file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js'), true);
-	}
-		
-	function setRates()
-	{
-	    $ar_rates = $this->cbrXMLDailyRu();
-	    $date = date('d.m.Y');
-	    $entity_data_class = $this->getEntityDataClass($this->id_exchange_rates);
-
-	    foreach($this->list_currency as $value){
-
-	        $car_rate = $ar_rates["Valute"][$value["UF_SHORT_NAME"]]["Value"]??1;
-
-	        $this->exchange_rates[$value["UF_SHORT_NAME"]] = [
-		    'UF_VALUE' => $car_rate,
-	        ];
-
-	        $entity_data_class::add([
-		    'UF_CURRENCY_ID' => $value["ID"],
-		    'UF_VALUE' => $car_rate,
-		    'UF_DATE' => $date,
-	        ]);
-	    }
-	}
-		
-	function getRates()
-	{
-	    $entity_data_class = $this->getEntityDataClass($this->id_exchange_rates);
-            $rsData = $entity_data_class::getList([
-	       "select" => array("UF_CURRENCY_ID", "UF_VALUE"),
-	       "order" => array("ID" => "ASC"),
-	       "filter" => array("UF_DATE"=>date('d.m.Y'))  // Задаем параметры фильтра выборки
-	    ]);
-			
-	    while($arData = $rsData->Fetch()){
-	        $this->exchange_rates[$this->list_currency[$arData["UF_CURRENCY_ID"]]["UF_SHORT_NAME"]]["UF_VALUE"] = $arData["UF_VALUE"];
-	    }
-		
-	    if (empty($this->exchange_rates)) {
-	        $this->setRates();
-	    }
-			
-	    return true;
-	}
-		
-	function getCurrency()
-	{
-	    $entity_data_class = $this->getEntityDataClass($this->id_list_currency);
-            $rsData = $entity_data_class::getList([
-	       "select" => array("*")
-	    ]);
-			
-	    while($arData = $rsData->Fetch()){
-	        $this->list_currency[$arData["ID"]] = $arData;
-	        $currency .= '{name:"'.$arData['UF_SHORT_NAME'].'", desc:"'.$arData['UF_CURRENCY_NAME'].'"},';
-	    }
-		
-	    $this->arResult["LIST_CURRENCY"] = '['.$currency.']';
-	}
+                
+            return true;
+        }
+            
+        function getCurrency()
+        {
+            $entity_data_class = $this->getEntityDataClass($this->id_list_currency);
+                $rsData = $entity_data_class::getList([
+            "select" => array("*")
+            ]);
+                
+            while($arData = $rsData->Fetch()){
+                $this->list_currency[$arData["ID"]] = $arData;
+                $currency .= '{name:"'.$arData['UF_SHORT_NAME'].'", desc:"'.$arData['UF_CURRENCY_NAME'].'"},';
+            }
+            
+            $this->arResult["LIST_CURRENCY"] = '['.$currency.']';
+        }
 		
 
         function createListCurrency()
